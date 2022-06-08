48ac62eef2d8826b6657a265f643981d39a0572b
Mike <kosolapus@gmail.com>
2022-06-08 22:12:05 +0300

    bump

--- serverMiddleware/api-server.ts
+++ serverMiddleware/api-server.ts
@@ -1,6 +1,6 @@
 import * as url from 'url'
 
-export default async (req: any, res: any, next: any) => {
+export default async (req: any, res: any, _: any) => {
   let urlString = req._parsedUrl.pathname.replace(/^\/+|\/+$|\.+/g, '')
   urlString = urlString.split('/')
   const method = urlString.pop()
