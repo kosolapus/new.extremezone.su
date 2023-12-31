84f7d3a084d00e1f811f4406431a61df817e2efa
Mike Shved <kosolapus@gmail.com>
2023-12-31 05:06:26 +0300

    update map size

--- components/ui/MapComponent.vue
+++ components/ui/MapComponent.vue
@@ -1,22 +1,23 @@
 <template>
   <div class="map__content">
-    <script
-      type="text/javascript"
-      charset="utf-8"
-      async
-      src="https://yandex.ru/map-widget/v1/?um=constructor%3AN8BbEtWnE0f4MqP79NYSZYb_ObICoCUo&amp;source=constructor"
-    ></script>
+    <script type="text/javascript" charset="utf-8" async :src="src"></script>
   </div>
 </template>
 
 <script>
   export default {
     name: 'MapComponent',
+    data() {
+      return {
+        src: 'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3AN8BbEtWnE0f4MqP79NYSZYb_ObICoCUo&amp;width=100%25&amp;height=450&amp;lang=ru_RU&amp;scroll=true',
+      }
+    },
   }
 </script>
 
 <style scoped>
   .map__content {
     min-height: 450px;
+    height: 450px;
   }
 </style>
